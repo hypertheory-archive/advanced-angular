aa1f8e97459e941b187249dc027d0628425dd12f
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-25 12:04:47 -0400

    structure completed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppNgrxModule } from './app-ngrx.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, AppNgrxModule],
   providers: [],
   bootstrap: [AppComponent],
