3572ac51e622afe6dd5a3ab4e37dd60c041f196f
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-25 11:50:12 -0400

    before router state

--- frontend/src/app/app-ngrx.module.ts
+++ frontend/src/app/app-ngrx.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { externalModules } from './build-specifics';
 import { reducers } from './state';
-
+// This will be the configuration for all of our NGRX Stuff. This will have to be imported into the AppModule like we do with the routing module
 @NgModule({
-  imports: [StoreModule.forRoot(reducers), EffectsModule.forRoot([])],
+  imports: [
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([]),
+    externalModules,
+  ],
   exports: [],
 })
 export class AppNgrxModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppNgrxModule } from './app-ngrx.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, AppNgrxModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
