24cf445f492be0c31e353bb8be1fcb3496780ac4
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-27 15:30:56 -0400

    filtering done

--- frontend/src/app/features/todos/state/index.ts
+++ frontend/src/app/features/todos/state/index.ts
@@ -4,22 +4,33 @@ import {
   createSelector,
 } from '@ngrx/store';
 import * as fromModels from '../models';
+import * as fromItemsFilter from './reducers/items-filter.reducer';
 import * as fromItems from './reducers/items.reducer';
 export const featureName = 'featureTodos';
 export interface TodosState {
   items: fromItems.ItemsState;
+  itemsFilter: fromItemsFilter.ItemsFilterState;
 }
 
 export const reducers: ActionReducerMap<TodosState> = {
   items: fromItems.reducer,
+  itemsFilter: fromItemsFilter.reducer,
 };
 
 // 1. Feature Selector
 const selectFeature = createFeatureSelector<TodosState>(featureName);
 // 2. Selector per "branch" of this feature
 const selectItemsBranch = createSelector(selectFeature, (f) => f.items);
+const selectItemsFilterBranch = createSelector(
+  selectFeature,
+  (f) => f.itemsFilter
+);
 // 3. Any helpers (optional)
 
+const selectFilteringItemsBy = createSelector(
+  selectItemsFilterBranch,
+  (b) => b.filteringBy
+);
 const selectTodoItemEntities = createSelector(
   selectItemsBranch,
   fromItems.selectAllTodoItemEtities
@@ -32,22 +43,40 @@ const selectNumberOfTodoItems = createSelector(
 
 const selectTodoItemViewModels = createSelector(
   selectTodoItemEntities,
-  (items) => items as fromModels.TodoItemViewModel[]
+  selectFilteringItemsBy,
+  (items, filter) => {
+    if (filter === 'complete') {
+      return [...items.filter((i) => i.completed)];
+    }
+    if (filter === 'incomplete') {
+      return [...items.filter((i) => i.completed === false)];
+    }
+    return items;
+  }
+);
+
+const selectItemsStats = createSelector(
+  selectTodoItemEntities,
+  selectNumberOfTodoItems,
+  selectFilteringItemsBy,
+  (items, total, filter) =>
+    ({
+      total,
+      completed: items.filter((i) => i.completed).length,
+      incomplete: items.filter((i) => i.completed === false).length,
+      showing: filter,
+    } as fromModels.FilterButtonsViewModel)
 );
 // 4. What the component needs (Models)
 
 export const selectTodosViewModel = createSelector(
   selectTodoItemViewModels,
-  (items) =>
+  selectItemsStats,
+  (items, filterButtons) =>
     ({
-      clearCompletedEnabled: true,
+      clearCompletedEnabled: filterButtons.completed > 0,
       dataLoaded: true,
-      filterButtons: {
-        completed: 1,
-        incomplete: 2,
-        total: 3,
-        showing: 'all',
-      },
+      filterButtons,
       items,
     } as fromModels.TodosViewModel)
 );
